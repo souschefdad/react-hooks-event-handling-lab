74c0aa75730437336f26a2036eef347793013eda
Pierce <piercedavidraphael@gmail.com>
2021-11-09 08:43:47 -0600

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+function Keypad() {
+    return (
+        <form>
+            <label for={'code-box'}>Enter Password </label>
+            <input type='password' id='code-box' onChange={() => console.log('Entering password...')}/>
+        </form>
+    )
+}
+
+export default Keypad
